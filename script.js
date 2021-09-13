51675c172ec95e88892e5d25221a9ebe0a3fef7b
07nikk <nikhilthakur0698@gmail.com>
2021-09-13 19:07:56 +0530

    uploading my files

---
+++ script.js
@@ -0,0 +1,22 @@
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+// console.log(color1);
+// console.log(color2);
+// console.log(body);
+
+function setGradient()
+{
+	body.style.
+
+	// body.style.background = 
+	// "linear-gradient(to right, "
+	// +color1.value
+	// +","
+	// +color2.value
+	// +")";
+}
+
+color1.addEventListener("input",setGradient);
+color1.addEventListener("input",setGradient);
